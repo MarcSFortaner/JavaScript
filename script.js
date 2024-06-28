658eb0394d6c5b4524a7509d995de263eb21e097
MarcSFortaner <marcsf_@hotmail.com>
2024-06-28 21:20:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log(userTitle); console.log(userName); console.log(userSurname);
+console.log(userTitle, userName, userSurname);
+userName = "Pablo";
+console.log(userTitle + ' + userName + + userSurname); console.log(userTitle + ESPACIO + userName + ESPACIO + usersurname);
+userSurname = "Rodriguez";
+console.log(¿Qué tal estás, ${userName) $(userSurname)?");
+userAge 33; // Number
+userExperience = 10;
+userHasHair = false; // Boolean
+console.log(userName + userAge);
+console.log(userAge + userExperience);
+userExperience = "diez";
+console.log(userAge + userExperience);
+console.log("Edad: + userAge, "\nAños de experiencia: " + userExperience); I
+// ESPACIO = "-";
+// TODO: arrays, objects
